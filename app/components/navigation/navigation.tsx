0dd86912397370b90ac2bc45d956e8adc2f00c04
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-05 22:32:56 +0100

    ajout d'un projet regroupant les differents composants react a réutilisé

---
+++ app/components/navigation/navigation.tsx
@@ -0,0 +1,19 @@
+"use client"
+import Link from "next/link";
+import { ActiveLink } from "./active-link";
+import './styles-navigation.css'
+/* eslint-disable react/no-unescaped-entities */
+
+export default function Navigation() {
+  return (
+
+    <nav className="menu">
+      <ActiveLink href="/">Accueil</ActiveLink>
+      <ActiveLink href="/pre-inscription">Pré-inscription</ActiveLink>
+      <ActiveLink href="/quiz">Plus d'Infos</ActiveLink>
+      <ActiveLink href="/actu">Actualités</ActiveLink>
+      <ActiveLink href="/contact">Contact</ActiveLink>
+    </nav>
+
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import {NavBar} from '@/app/components/nav-bar/nav'
+import { ScrollingText } from './components/scrolling-text/scrolling-text'
+import Navigation from './components/navigation/navigation'
 
 export default function Home() {
   return (
     <>
       <NavBar/>
+      <Navigation/>
+      <ScrollingText/> 
     </>
   )
 }
